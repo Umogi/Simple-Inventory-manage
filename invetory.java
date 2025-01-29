35ff8e6a6c64182002025b52ef93c8075e9c31da
kelly <152154777+Umogi@users.noreply.github.com>
2025-01-29 23:12:18 +0200

    Update invetory.java

--- invetory.java
+++ invetory.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class invetory {
+    Scanner scanner = new Scanner(System.in);
     private ArrayList<items> invetories;
 
     public invetory(){
@@ -12,10 +14,74 @@ public class invetory {
     }
 
     public void viewItems(){
+        System.err.println("===== View Item =====");
         for (items item : invetories){
             System.out.println(item);
         }
     }
 
-    
+    public void upDateItem(){
+        System.err.println("===== UpDate Item =====");
+        for (items item : invetories){
+            System.out.println(item);
+        }
+        System.out.print("Piick item ID: ");
+        int choice = scanner.nextInt();
+
+        for (items item : invetories){
+            if (item.getID() == choice){
+                System.out.println("Item Amount: " + item.getQuantity());
+                System.out.print("New Amount: ");
+                int newAmount = scanner.nextInt();
+                item.setQuantity(newAmount);
+                break;
+            }
+        }
+    }
+
+    public void upDatePrice(){
+        System.err.println("===== UpDate price =====");
+        for (items item : invetories){
+            System.out.println(item);
+        }
+        System.out.print("Piick item ID: ");
+        int choice = scanner.nextInt();
+
+        for (items item : invetories){
+            if (item.getID() == choice){
+                System.out.println("Item Price: " + item.getPrice());
+                System.out.print("New Price: ");
+                int newAmount = scanner.nextInt();
+                item.setPrice(newAmount); 
+                break;
+            }
+        }
+    }
+
+    public void deleteItem(){
+        System.err.println("===== Delete Item =====");
+        for (items item : invetories){
+            System.out.println(item);
+        }
+        System.out.println("Pick item ID: ");
+        int choice = scanner.nextInt();
+        for (items item : invetories){
+            if(item.getID() == choice){
+                item.setQuantity(0);
+                System.out.println("This item didn't got delete. Quanity set to 0!");
+                break;
+            }
+        }
+    }
+
+    public void searchItem(){
+        System.out.println("===== Search Item =====");
+        String searchItem = scanner.nextLine();
+
+        for (items item : invetories){
+            if (item.getName().equals(searchItem)){
+                System.out.println(item);
+            }
+        }
+    }
 }
